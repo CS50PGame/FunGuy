9779fd7530695c4e162eb556b823a95ad1582b5a
uaciej <amacedmace@gmail.com>
2023-03-26 12:13:50 +0200

    FUN-4 GameLoop updated to Game class and 'run' method

--- code/main.py
+++ code/main.py
@@ -1,57 +1,60 @@
 import pygame
 from characters import Player
+from settings import *
 
-# Initialize Pygame
-pygame.init()
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('FunGuy')
+        self.clock = pygame.time.Clock()
 
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Simple')
+    def draw_title_screen(self):
+        self.screen.fill(('black'))
+        font = pygame.font.Font(None, 64)
+        title_text = font.render("FunGuy", True, (255, 0, 0))
+        font = pygame.font.Font(None, 16)
+        subtitle_text = font.render("Press any key to start", True, (0, 0, 255))
+        title_rect = title_text.get_rect()
+        title_rect.center = self.screen.get_rect().center
+        subtitle_rect = subtitle_text.get_rect()
+        subtitle_rect.center = (title_rect.center[0], title_rect.center[1] + 100)
+        self.screen.blit(title_text, title_rect)
+        self.screen.blit(subtitle_text, subtitle_rect)
+        pygame.display.flip()
 
-clock = pygame.time.Clock()
+    def run(self):
+        self.draw_title_screen()
 
-# Title Screen
-font = pygame.font.Font(None, 64)
-title_text = font.render("FunGuy", True, (255, 0, 0))
-font = pygame.font.Font(None, 16)
-subtitle_text = font.render("Press any key to start", True, (0, 0, 255))
-title_rect = title_text.get_rect()
-title_rect.center = screen.get_rect().center
-subtitle_rect = subtitle_text.get_rect()
-subtitle_rect.center = (title_rect.center[0], title_rect.center[1] + 100)
-screen.blit(title_text, title_rect)
-screen.blit(subtitle_text, subtitle_rect)
-pygame.display.flip()
+        # Wait for input to start the game loop
+        waiting = True
+        while waiting:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN or event.type == pygame.QUIT:
+                    waiting = False
 
-# Wait for input to start the game loop
-waiting = True
-while waiting:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN or event.type == pygame.QUIT:
-            waiting = False
+        pygame.display.update()
+        player = pygame.sprite.GroupSingle()
+        player.add(Player())
+        player.draw(self.screen)
 
-# Initialize game screen and player
-screen.fill((255, 255, 255))
-pygame.display.update()
-player = pygame.sprite.GroupSingle()
-player.add(Player())
-player.draw(screen)
+        # Game loop
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
 
-# Game loop
-running = True
-while running:
-    # Handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+            # Move player and redraw the screen
+            player.update()
+            self.screen.fill((255, 255, 255))
+            player.draw(self.screen)
+            pygame.display.update()
+            self.clock.tick(FPS)
 
-    # Move player and redraw the screen
-    player.update()
-    screen.fill((255, 255, 255))
-    player.draw(screen)
-    pygame.display.update()
-    clock.tick(60)
+        # Quit Pygame
+        pygame.quit()
 
-# Quit Pygame
-pygame.quit()
+if __name__=="__main__":
+    game = Game()
+    game.run()
